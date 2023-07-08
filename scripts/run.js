85a8426498d2a4e19723749085e8ca1b37d95d71
Charles M Dellinger <charles.dellinger@gmail.com>
2023-07-08 09:45:36 -0400

    created a contract that mints the same NFT successfully to local testnet

--- scripts/run.js
+++ scripts/run.js
@@ -4,8 +4,18 @@ const main = async () => {
     const nftContract = await ethers.deployContract('MyEpicNFT');
     await nftContract.waitForDeployment();
     console.log("Contract deployed to:", await nftContract.getAddress());
+
+    // mint an NFT
+    let txn = await nftContract.makeAnEpicNFT()
+    // wait for mint
+    await txn.wait()
+
+    // mint another NFT
+    txn = await nftContract.makeAnEpicNFT()
+    // wait for mint
+    await txn.wait()
   };
-  
+
   const runMain = async () => {
     try {
       await main();
